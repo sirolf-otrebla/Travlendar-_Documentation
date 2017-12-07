e709f6ce0349a020954dd9f4c72cbe4f526fa100
Luca Napoletano <10464485@polimi.it>
2017-12-07 13:19:31 +0100

    First implementation of the Web Server

---
+++ src/travlendar/node/www/server.js
@@ -0,0 +1,19 @@
+//WEB SERVER REQUIRES
+var express = require('express');
+var path = require('path');
+
+//WEB SERVER DEFINITIONS
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+//ROUTES DEFINITION
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+//STARTING THE SERVER
+var port = '80';
+app.listen(port);
+console.log('The web server is working on the port: ' + port);
