5f665e8b73493d03529fdd441517815517a834e4
sirolf-otrebla <albeflor@gmail.com>
2017-12-08 17:31:36 +0100

    data access manager sketch

--- src/travlendar/node/app/logic/data_acc/dataManager.js
+++ src/travlendar/node/app/logic/data_acc/dataManager.js
@@ -1,44 +1,87 @@
-/*
-var net = require('net');
-var fs = require('fs');
-
-var servers = JSON.parse(fs.readFileSync('./travlendar/node/www/dispatcher/file.json')).servers;
-
-var dispatcher = new net.Socket();
-
-dispatcher.connect(servers[0].port, servers[0].address, function() {
-    console.log('connected');
-    dispatcher.write('something');
-});
-
-dispatcher.on('data', function(data) {
-    console.log(data.toString());
-});
- */
 
 
-DBConn = {
-    __self : this,
-    __socket : null,
 
+function DataConn(ID){
+    var __self  = this;
+    let net = require('net');
+    let fs = require('fs');
+    let serverList = JSON.parse(fs.readFileSync('./travlendar/node/app/data_acc/serverList.json')).servers;
 
-    Connection : function (ID) {
-        let net = require('net');
-        let fs = require('fs');
-        let serverList = JSON.parse(fs.readFileSync('./travlendar/node/app/data_acc/serverList.json')).servers;
-        let connectingTo = serverList[ID];
-        __socket = new net.socket();
-        __connect(connectingTo);
-    },
-
-    write : function (msg) {
-        __socket.write(msg);
-    },
-
-    __connect : function (connectingTO) {
-        this.__socket.connect(connectingTO.port, connectingTO.address, function () {
-            console.log("CONNECTED TO : " + connectingTO.port + ":" + connectingTO.address);
-        })
+    this.__connectingTO = serverList[ID];
+    this.__socket = new net.socket();
+    
+    this.connect = function () {
+        this.__socket.connect(this.__connectingTO.port, this.__connectingTO.address, function () {
+            console.log("CONNECTED TO : " + __self.__connectingTO.port + ":" + __self.__connectingTO.address);
+        });
     }
+
+    this.write = function (msg) {
+        this.__socket.write(msg);
+    };
+
+};
+
+function DataAccess(){
+
+    const MY_DATA_SERVER_ID = 0;
+    const MY_EXT_SERVER_ID = 1;
+    
+    this.__DBConn = new DataConn(MY_DATA_SERVER_ID);
+    this.__extConn = new DataConn(MY_EXT_SERVER_ID);
+    
+    this.fetchUser = function (email, ID) {
+        let msg = {
+
+            fetch : function (dbRef) {
+
+            }
+        }
+        //TODO
+    }
+    this.fetchTasks = function (user) {
+        let msg = {
+
+            fetch : function (dbRef) {
+
+            }
+        }
+        //TODO
+    }
+
+    this.addTask = function (user, task) {
+        let msg = {
+
+
+            fetch : function (dbRef) {
+
+            }
+        }
+        //TODO
+    }
+
+    this.removeTask = function (user, task) {
+        let msg = {
+
+            fetch : function (dbRef) {
+
+            }
+        }
+        //TODO
+    }
+
+    this.getTravelCost = function (origin, dest, time) {
+        let msg = {
+
+            fetch : function (dbRef) {
+
+            }
+        }
+        //TODO
+    }
+
+
+
+
 
 }
