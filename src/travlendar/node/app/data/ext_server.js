18567308ca662ffca4c3d6343040916a2926080d
sirolf-otrebla <albeflor@gmail.com>
2017-12-10 16:32:18 +0100

    Merge branch 'master' of https://github.com/lnapo94/FlorisMontanariNapoletano
    # Conflicts:
    #	src/travlendar/node/app/logic/data_acc/dataManager.js

---
+++ src/travlendar/node/app/data/ext_server.js
@@ -0,0 +1,25 @@
+const EXT_FETCHER_PORT = 12345;
+const EXT_FETCHER_IP = 'localhost';
+
+let net = require('net');
+
+let server = net.createServer( (socket) => {
+    console.log("data access manager connected");
+    
+    socket.on('end', function () {
+        console.log("data access manager disconnected");
+    });
+    
+    socket.on('data', function (data) {
+        let ext = require('./external.js');
+        let obj = JSON.parse(data);
+        try{
+            obj.fetch(ext.fetchRoute);
+            socket.write(JSON.stringify(obj));
+        } catch (err){
+            //TODO
+        }
+    })
+});
+
+server.listen(EXT_FETCHER_PORT, EXT_FETCHER_IP);
