5c97e1d5124108a0d346948dc9e8814ab0ecf4d1
Luca Napoletano <10464485@polimi.it>
2017-12-29 17:03:45 +0100

    Fixed the login and added the user personal page

--- src/travlendar/node/www/routes/index.js
+++ src/travlendar/node/www/routes/index.js
@@ -1,7 +1,6 @@
 let express = require('express');
+let loginFunction = require('./loginFunction');
 let router = express.Router();
-let dispatcher = require('../dispatcher/dispatcher');
-
 
 //GET HOME PAGE
 router.get('/', function(req, res) {
@@ -9,23 +8,7 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-   let data = JSON.stringify({"email": req.body.email, "password": req.body.password, "requestedService": "login"})
-   dispatcher.sendMessage(req.body.email, data, function(message) {
-
-      let json = JSON.parse(message);
-
-      if(json.errorType !== undefined) {
-         res.send('Error in Login\nError: ' + json.errorType);
-      } else {
-         if(json.loggedIn === 'true') {
-            res.send("User is logged in");
-         }
-         else {
-            res.send("User isn't logged in");
-         }
-      }
-
-   });
+   loginFunction.checkUser(req, res);
 });
 
 module.exports = router;
